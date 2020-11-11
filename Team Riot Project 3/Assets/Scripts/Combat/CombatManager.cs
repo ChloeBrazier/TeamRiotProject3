e3bbd4ab5990f05395a4616a84b38f64cdc8bcac
tgraha09 <tfire09@gmail.com>
2020-11-11 16:49:09 -0500

    Revert "UI changes"
    This reverts commit 3a310f4bab722ec8e8a252e871d1453b3936f1ad.

--- Team Riot Project 3/Assets/Scripts/Combat/CombatManager.cs
+++ Team Riot Project 3/Assets/Scripts/Combat/CombatManager.cs
@@ -25,23 +25,6 @@ public class CombatManager : MonoBehaviour
     float move_width;
     float move_height;
     GameObject[] tiles;
-    //Menus
-    public GameObject combatmenu;
-    public GameObject elementmenu;
-    public GameObject movemenu;
-    public GameObject fleemenu;
-
-    
-
-    bool w = false;
-    bool a = false;
-    bool s = false;
-    bool d = false;
-
-    bool player_turn = false;
-    int player_lvl = 0;
-    float xp_nextlvl = 100.0f;
-    v3 menu_pos;
     public enum combatOptions
     {
         move, attack, flee, none
@@ -55,7 +38,6 @@ public class CombatManager : MonoBehaviour
         cam = GameObject.FindGameObjectWithTag("MainCamera");
         mainCam = cam.GetComponent<Camera>();
         tiles = GameObject.FindGameObjectsWithTag("tile");
-<<<<<<< Updated upstream
         GameObject starttile = tiles[0];
         currentPlayerTileIndex = 0;
         var spr_render = starttile.GetComponent<SpriteRenderer>().bounds.size;
@@ -65,117 +47,68 @@ public class CombatManager : MonoBehaviour
         //n_p.z = -3;
         Player.transform.position = starttile.transform.position;
         Enemy.transform.position = tiles[14].transform.position;
-=======
-        
-        GameObject starttile = tiles[13];
-        var spr_render = starttile.GetComponent<SpriteRenderer>().bounds.size;
-        move_width = spr_render.x;
-        move_height = spr_render.z;
-        var n_p = starttile.transform.position;
-        n_p.y = -2;
-        Player.transform.position = n_p;
->>>>>>> Stashed changes
         currentOpt = combatOptions.none;
         Debug.Log(currentOpt);
-        CheckIntersectXY(Player.transform.position, starttile.transform.position);
-        player_turn = true; 
+        CheckIntersect(Player, starttile);
+
        // mainCam.rect.width;
-        
-
-      
-        //Destroy(reftile);
-
-
-        // Update is called once per frame
-
-        if (player_turn == true)
+        /*if(right == null && left == null)
         {
-            Debug.Log(player_turn);
-            elementmenu.SetActive(false);
-            movemenu.SetActive(false);
-            fleemenu.SetActive(false);
-        }
+            right = (GameObject)Resources.Load("tile_right");
+            left = (GameObject)Resources.Load("tile_left");
+            reftile = (GameObject)Resources.Load("tile");
+        }*/
 
-    }
-
-    public void AttackOption()
-    {
-        Debug.Log("Attack");
-        //Instantiate(elementmenu);
-        elementmenu.SetActive(true);
-        combatmenu.SetActive(!combatmenu.activeSelf);
-        currentOpt = combatOptions.attack;
-    }
-
-    public void MoveOption()
-    {
-        Debug.Log("Move");
-        movemenu.SetActive(true);
-        combatmenu.SetActive(!combatmenu.activeSelf);
-        currentOpt = combatOptions.move;
-    }
-
-    public void FleeOption()
-    {
-        Debug.Log("Flee");
-        fleemenu.SetActive(true);
-        combatmenu.SetActive(!combatmenu.activeSelf);
-        currentOpt = combatOptions.flee;
-    }
-
-    public void Back()
-    {
-        Debug.Log("Back");
-        switch (currentOpt)
+        void GenFloor()
         {
-            case combatOptions.move:
-                    movemenu.SetActive(false);
-                    combatmenu.SetActive(!combatmenu.activeSelf);
-                    currentOpt = combatOptions.none;
-                break;
-            case combatOptions.attack:
-                    elementmenu.SetActive(false);
-                    combatmenu.SetActive(!combatmenu.activeSelf);
-                    currentOpt = combatOptions.none;
-                break;
-            case combatOptions.flee:
-                    fleemenu.SetActive(false);
-                    combatmenu.SetActive(!combatmenu.activeSelf);
-                    currentOpt = combatOptions.none;
-                break;
-            case combatOptions.none:
-                break;
-            default:
-                break;
-        }
-    }
-
-    void Update()
-    {
-        if (player_turn == true)
-        {
-            switch (currentOpt)
+            var cw = mainCam.rect.width;
+            var ch = mainCam.rect.height;
+            Debug.Log(mainCam.pixelWidth);
+            Debug.Log(mainCam.pixelHeight);
+            Debug.Log(ch);
+            var id = 0;
+            //Debug.Log(reftile.GetComponent<SpriteRenderer>().size);
+            for (int c = 0; c < cols; c++)
             {
-                case combatOptions.move:
+                for (int r = 0; r < rows; r++)
+                {
 
+                    var tile = (GameObject)Instantiate(reftile, transform);
+                    var size = tile.GetComponent<SpriteRenderer>().size;
+                    var sprite = tile.GetComponent<SpriteRenderer>().sprite;
+                    
                     
 
-                    if (Input.GetKey(KeyCode.W) && w == false)
+                    float posX = (r * size.x) + cw;
+                    float posY = (c * -size.y) + ch;
+                    
+                    tile.transform.position = new v3(posX, posY, -2);
+                    var rot = tile.transform.rotation;
+                    
+                    //var d_c = cols - c;
+                    //float diff = (float)(d_r * d_c);
+                    if(r < 3)
                     {
-                        print("W");
+                        
+
+                    }
+                    if (r >= 3)
+                    {
+                       // var d_r = r * 6;
+                        //var n_p = new v3(posX, posY, -2);
+                        //tile.transform.Translate(new v3(-1, 0, 0));
+                        //tile.transform.position = n_p;
+                        //tile.transform.Rotate(new v3(0f, 1f, 0), d_r);
+
                         
                     }
-                    if (Input.GetKey(KeyCode.S))
+                   /* if (c < 2)
                     {
-                        print("S");
+                        var d_c = (cols - c) * 4;
+                        tile.transform.Rotate(new v3(1f, 0, 0), -d_c);
                     }
-                    if (Input.GetKey(KeyCode.A))
+                    if (c >= 2)
                     {
-                        print("A");
-                    }
-                    if (Input.GetKey(KeyCode.D))
-                    {
-<<<<<<< Updated upstream
                         var d_c = c * 4;
                         tile.transform.Rotate(new v3(1f, 0, 0), d_c);
                     }*/
@@ -253,40 +186,15 @@ public class CombatManager : MonoBehaviour
             }
         }
     }
-=======
-                        print("D");
-                    }
-                    foreach (var tile in tiles)
-                    {
-                        
-                    }
-                        
-                    break;
-                case combatOptions.attack:
-                    break;
-                case combatOptions.flee:
-                    break;
-                case combatOptions.none:
-                    break;
-                default:
-                    break;
-            }
-        }
-        
->>>>>>> Stashed changes
 
     void EnemyMove() 
     {
         
     }
-<<<<<<< Updated upstream
     private bool CheckIntersect(GameObject obj1, GameObject obj2)
-=======
-
-    private bool CheckIntersectXY(v3 p1, v3 p2)
->>>>>>> Stashed changes
     {
-    
+        var p1 = obj1.transform.position;
+        var p2 = obj2.transform.position;
         var x = p1.x / p2.x;
         var y = p1.y / p2.y;
         if(x > 0.5 || y > 0.5)
