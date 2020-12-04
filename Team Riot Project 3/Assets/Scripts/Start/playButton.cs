a4db6677e55b3243c917122cd37dc36df77dde93
Eddie Brazier <edwinbrazier01@gmail.com>
2020-12-04 14:15:55 -0500

    added credits section to main menu

--- Team Riot Project 3/Assets/Scripts/Start/playButton.cs
+++ Team Riot Project 3/Assets/Scripts/Start/playButton.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class playButton : MonoBehaviour
 {
     public Text text;
+    public List<GameObject> buttons;
+    public List<GameObject> credits;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,5 +36,38 @@ public class playButton : MonoBehaviour
     {
         text.color = Color.black;
         SceneManager.LoadScene(sceneName: "OverworldScene");
+    }
+
+    public void CreditsClick()
+    {
+        text.color = Color.white;
+
+        //disable main menu buttons
+        foreach (GameObject button in buttons)
+        {
+            button.SetActive(false);
+        }
+
+        //enable credits menu
+        foreach (GameObject creditsItem in credits)
+        {
+            creditsItem.SetActive(true);
+        }
+    }
+
+    public void CreditsExit()
+    {
+        text.color = Color.white;
+
+        //disable credits menu and re-enable main menu buttons
+        foreach (GameObject creditsItem in credits)
+        {
+            creditsItem.SetActive(false);
+        }
+
+        foreach (GameObject button in buttons)
+        {
+            button.SetActive(true);
+        }
     }
 }
